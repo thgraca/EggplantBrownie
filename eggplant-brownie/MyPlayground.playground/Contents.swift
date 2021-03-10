b4c1cc895972151764e592c892fbb45179b74e70
thgraca <thgraca@gmail.com>
2021-03-10 12:19:42 -0300

    Utilizando array dentro da minha classe de almoço

--- eggplant-brownie/MyPlayground.playground/Contents.swift
+++ eggplant-brownie/MyPlayground.playground/Contents.swift
@@ -8,22 +8,51 @@ import UIKit
 
 
 class Refeicao{
-    var nome: String?
-    var felicidaide: String?
+    var nome: String
+    var felicidade: String
+    var itens: Array<Item> = []
     
-}
-
-let refeicao = Refeicao()
-refeicao.nome = "Churros do chaves"
-
-func exibeNomeDaRefeicao() {
-    guard let nome = refeicao.nome else {
-        return
+    
+    
+    init(nome: String, felicidade: String) {
+        self.nome =  nome
+        self.felicidade = felicidade
     }
     
-    print(nome)
+    func totalDeCalorias() -> Double {
+        var total = 0.0
+        
+        for item in itens {
+            total += item.calorias
+        }
+        
+        return total
+    }
+
+}
+
+class Item {
+    var nome: String
+    var calorias: Double
     
+    init(nome: String, calorias: Double) {
+        self.nome = nome
+        self.calorias = calorias
+    }
 }
 
 
-exibeNomeDaRefeicao()
+let refeicao = Refeicao(nome: "Almoço",felicidade:  "5")
+
+let arroz = Item(nome: "Arroz", calorias: 50.0)
+let feijao = Item(nome: "Feijão", calorias: 80.0)
+let contraFile = Item(nome: "Contra Filé", calorias: 200.0)
+refeicao.itens.append(arroz)
+refeicao.itens.append(feijao)
+refeicao.itens.append(contraFile)
+
+    
+print(refeicao.nome)
+
+print(refeicao.totalDeCalorias())
+    
